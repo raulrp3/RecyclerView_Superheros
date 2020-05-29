9f1b8ec21ed208a5bb8718041fe16021a3de98ef
Raúl Ramírez <raul.ramirez@escuelaestech.es>
2020-05-29 13:19:54 +0200

    Mostrando el detalle de un superhero

---
+++ app/src/main/java/com/example/recyclerviewapplication/DetailAcitivity.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerviewapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_acitivity)
+    }
+}
